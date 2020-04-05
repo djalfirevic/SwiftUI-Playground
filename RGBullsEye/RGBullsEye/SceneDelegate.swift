f7a53f2fa6789b0c06a3829dcc1757585e256cdc
Djuro Alfirevic <djalfirevic@gmail.com>
2020-04-05 08:29:54 +0200

    RGBullsEye project added

---
+++ RGBullsEye/RGBullsEye/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  RGBullsEye
+//
+//  Created by Djuro Alfirevic on 4/5/20.
+//  Copyright © 2020 Djuro Alfirevic. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    // MARK: - Properties
+    var window: UIWindow?
+
+    // MARK: - UIWindowSceneDelegate
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+}
---
+++ RGBullsEye/RGBullsEye/Views/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Djuro Alfirevic on 4/5/20.
+//  Copyright © 2020 Djuro Alfirevic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    @State var showAlert = false
+    
+    func computeScore() -> Int {
+        let rDiff = rGuess - rTarget
+        let gDiff = gGuess - gTarget
+        let bDiff = bGuess - bTarget
+        let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
+        
+        return Int((1.0 - diff) * 100.0 + 0.5)
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color")
+                }
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess * 255.0)) G: \(Int(gGuess * 255.0)) B: \(Int(bGuess * 255.0))")
+                }
+            }
+            
+            Button(action: {
+                self.showAlert = true
+            }) {
+                Text("Hit me!")
+            }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text(String(computeScore())))
+            }.padding()
+            
+            VStack {
+                ColorSlider(value: $rGuess, textColor: .red)
+                ColorSlider(value: $gGuess, textColor: .green)
+                ColorSlider(value: $bGuess, textColor: .blue)
+            }
+        }
+    }
+}
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255").foregroundColor(textColor)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+    }
+}
