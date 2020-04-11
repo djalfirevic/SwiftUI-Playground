cd1743daa41eae6e18f018ed4a3211b6988deec2
Djuro Alfirevic <djalfirevic@gmail.com>
2020-04-11 10:22:23 +0200

    SwiftUIStacks project added

---
+++ SwiftUIStacks/SwiftUIStacks/Views/ContentView.swift
@@ -0,0 +1,104 @@
+//
+//  ContentView.swift
+//  SwiftUIStacks
+//
+//  Created by Djuro Alfirevic on 4/11/20.
+//  Copyright © 2020 Djuro Alfirevic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HeaderView()
+            
+            HStack(spacing: 15) {
+                PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: Color.purple)
+                ZStack {
+                    PricingView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+                    
+                    Text("Best for designer")
+                        .font(.system(.caption, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(5)
+                        .background(Color(red: 255/255, green: 183/255, blue: 27/255))
+                    .offset(x: 0, y: 87)
+                }
+            }
+            .padding(.horizontal)
+            
+            ZStack {
+                PricingView(title: "Team", price: "$299", textColor: .white, bgColor: Color(red: 62/255, green: 63/255, blue: 70/255), icon: "wand.and.rays")
+                    .padding()
+                
+                Text("Best for teams with 20 members")
+                    .font(.system(.caption, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(5)
+                    .background(Color(red: 255/255, green: 183/255, blue: 27/255))
+                .offset(x: 0, y: 87)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Choose")
+                    .font(.system(.largeTitle))
+                    .fontWeight(.black)
+                Text("Your Plan")
+                    .font(.system(.largeTitle))
+                    .fontWeight(.black)
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct PricingView: View {
+    var title: String
+    var price: String
+    var textColor: Color
+    var bgColor: Color
+    var icon: String?
+    
+    var body: some View {
+        VStack {
+            icon.map({
+                Image(systemName: $0)
+                    .font(.largeTitle)
+                    .foregroundColor(textColor)
+            })
+            
+            Text(title)
+                .font(.system(.title))
+                .fontWeight(.black)
+                .foregroundColor(textColor)
+            Text(price)
+                .font(.system(size: 40, weight: .heavy, design: .rounded))
+                .foregroundColor(textColor)
+            Text("per month")
+                .font(.headline)
+                .foregroundColor(textColor)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+        .padding(40)
+        .background(bgColor)
+        .cornerRadius(10)
+    }
+}
