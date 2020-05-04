4ab5e120ec852439ec34e97c767ba20020b8459a
Djuro Alfirevic <djalfirevic@gmail.com>
2020-05-04 19:34:28 +0200

    Create SearchBar from scratch with SwiftUI

--- SwiftUIToDoList/ToDoList/ToDoList/Views/SearchBar.swift
+++ SwiftUIToDoList/ToDoList/ToDoList/Views/SearchBar.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 import UIKit
 
-struct SearchBar: UIViewRepresentable {
+/*struct SearchBar: UIViewRepresentable {
     @Binding var text: String
     
     func makeCoordinator() -> Coordinator {
@@ -58,5 +58,73 @@ class Coordinator: NSObject, UISearchBarDelegate {
         searchBar.resignFirstResponder()
         searchBar.showsCancelButton = false
         searchBar.endEditing(true)
+    }
+}*/
+
+struct SearchBar: View {
+    @Binding var text: String
+    @State private var isEditing = false
+    
+    private var searchText: Binding<String> {
+        Binding<String>(
+            get: {
+                self.text.capitalized
+            }, set: {
+                self.text = $0
+            }
+        )
+    }
+
+    var body: some View {
+        HStack {
+            TextField("Search ...", text: searchText)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+
+            if isEditing {
+                Button(action: {
+                    self.isEditing = false
+                    self.text = ""
+                    
+                    // Dismiss the keyboard
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar(text: .constant(""))
     }
 }
