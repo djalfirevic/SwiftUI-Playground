3ce8dfada3db85d7f0b47afe1017361ded7302fb
Djuro Alfirevic <djalfirevic@gmail.com>
2020-05-21 17:09:41 +0200

    Added SwiftUIAppStore project

---
+++ SwiftUIAppStore/SwiftUIAppStore/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAppStore
+//
+//  Created by Djuro Alfirevic on 5/21/20.
+//  Copyright © 2020 Djuro Alfirevic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
