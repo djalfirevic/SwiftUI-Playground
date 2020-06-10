226610fb855831a80de479553693780de2c46601
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-10 18:28:39 +0200

    EmojiArt project added

---
+++ CS193p/EmojiArt/EmojiArt/Models/EmojiArt.swift
@@ -0,0 +1,37 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by Djuro Alfirevic on 6/10/20.
+//  Copyright © 2020 Djuro Alfirevic. All rights reserved.
+//
+
+import Foundation
+
+struct EmojiArt {
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int // offset from the center
+        var y: Int // offset from the center
+        var size: Int
+        let id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    private var uniqueEmojiId = 0
+    
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiId))
+    }
+}
---
+++ CS193p/EmojiArt/EmojiArt/Models/EmojiArtDocument.swift
@@ -0,0 +1,64 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Djuro Alfirevic on 6/9/20.
+//  Copyright © 2020 Djuro Alfirevic. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    static let palette = "⭐️⛅️🍎🌍🥨⚾️"
+    @Published private var emojiArt = EmojiArt()
+    @Published private(set) var backgroundImage: UIImage?
+    
+    var emojis: [EmojiArt.Emoji] {
+        return emojiArt.emojis
+    }
+    
+    // MARK: Intent(s)
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        emojiArt.addEmoji(emoji, x: Int(location.x), y: Int(location.y), size: Int(size))
+    }
+    
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].x += Int(offset.width)
+            emojiArt.emojis[index].y += Int(offset.height)
+        }
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        if let index = emojiArt.emojis.firstIndex(matching: emoji) {
+            emojiArt.emojis[index].size = Int((CGFloat(emojiArt.emojis[index].size) * scale).rounded(.toNearestOrEven))
+        }
+    }
+    
+    func setBackgroundURL(_ url: URL?) {
+        emojiArt.backgroundURL = url?.imageURL
+        
+        fetchBackgroundImageData()
+    }
+    
+    private func fetchBackgroundImageData() {
+        backgroundImage = nil
+        
+        if let url = self.emojiArt.backgroundURL {
+            DispatchQueue.global(qos: .userInitiated).async {
+                if let data = try? Data(contentsOf: url) {
+                    DispatchQueue.main.async {
+                        if url == self.emojiArt.backgroundURL {
+                            self.backgroundImage = UIImage(data: data)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension EmojiArt.Emoji {
+    var fontSize: CGFloat { CGFloat(size) }
+    var location: CGPoint { CGPoint(x: CGFloat(x), y: CGFloat(y)) }
+}
