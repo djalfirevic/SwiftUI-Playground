493d6ef22a04bf08e3a51f2eab45e373526d52dc
Djuro Alfirevic <djalfirevic@gmail.com>
2021-08-26 19:29:53 +0200

    Add StretchyHeader project from https://medium.com/swlh/swiftui-create-a-stretchable-header-with-parallax-scrolling-4a98faeeb262

---
+++ StretchyHeader/StretchyHeader/StretchyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchyHeaderApp.swift
+//  StretchyHeader
+//
+//  Created by Djuro on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct StretchyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
